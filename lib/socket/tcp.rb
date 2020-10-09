de77de32353a3899397e1ff420075293aa43577f
metapsj <patrickjohnson@forty3sixteen.com>
2020-10-08 18:01:39 -0700

    implement simple looping, fiber, threading, and forking servers

--- lib/socket/tcp.rb
+++ lib/socket/tcp.rb
@@ -1,24 +1,101 @@
-
 require 'socket'
 
-server = TCPServer.new(9000)
+# looping server
+server = TCPServer.new('localhost', 9090)
 
 loop do
   client = server.accept
-  client.puts "Hello!"
-  client.puts "Time is #{Time.now}"
+
+  while buffer = client.gets
+    client.puts(buffer)
+  end
+
   client.close
 end
 
-__END__
+# fiber server
+require 'fiber'
 
-# threaded server
+class Reactor
+  def initialize
+    @readable = {}
+    @writable = {}
+  end
+
+  def run
+    while @readable.any? or @writable.any?
+      readable, writable = IO.select(@readable.keys, @writable.keys, [])
+      readable.each { |io| @readable[io].resume }
+      writable.each { |io| @writable[io].resume }
+    end
+  end
+
+  def wait_readable(io)
+    @readable[io] = Fiber.current
+    Fiber.yield
+    @readable.delete(io)
+    return yield if block_given?
+  end
+
+  def wait_writable(io)
+    @writable[io] = Fiber.current
+    Fiber.yield
+    @writable.delete(io)
+    return yield if block_given?
+  end
+end
+
+server = TCPServer.new('localhost', 9090)
+reactor = Reactor.new
+
+Fiber.new do
+  loop do
+    client = reactor.wait_readable(server) { server.accept }
+
+    Fiber.new do
+      while buffer = reactor.wait_readable(client) { client.gets }
+        reactor.wait_writable(client)
+        client.puts(buffer)
+      end
+
+      client.close
+    end.resume
+  end
+end.resume
+
+reactor.run
+
+# threading server
+server = TCPServer.new('localhost', 9090)
 
 loop do
-  Thread.start(server.accept) do |client|
-    client.puts "Hello!"
-    client.puts "Time is #{Time.now}"
+  client = server.accept
+
+  Thread.new do
+    while buffer = client.gets
+      client.puts(buffer)
+    end
+
     client.close
   end
 end
+
+
+# forking server
+server = TCPServer.new('localhost', 9090)
+
+loop do
+  client = server.accept
+
+  fork do
+    while buffer = client.gets
+      client.puts(buffer)
+    end
+
+    client.close
+  end
+
+  client.close
+end
+
 
